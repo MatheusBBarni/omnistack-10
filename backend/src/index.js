5e4761904535d4e028f6c1005667653a0a8b64a4
matheusBBarni <matheus@demacode.com.br>
2020-07-19 02:55:28 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const port = 3333;
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:barni789123@cluster0.lb19b.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(port);
---
+++ backend/src/utils/parseStringAsArray.js
@@ -0,0 +1,1 @@
+module.exports = arrayString => arrayString.split(',').map(tech => tech.trim());
